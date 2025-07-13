40c078f732074bddbefe9ce6fbe594b83ea56d57
Per Stark <per@stark.pub>
2025-07-13 22:24:08 +0200

    update to dev 2.0 repo

--- flake.nix
+++ flake.nix
@@ -4,42 +4,50 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
           (self: super: {
-            python3Packages = super.python3Packages // {
-              BlinkStick = pkgs.python3Packages.buildPythonPackage rec {
-                pname = "blinkstick";
-                version = "1.2.0"; 
+            python3Packages =
+              super.python3Packages
+              // {
+                BlinkStick = pkgs.python3Packages.buildPythonPackage rec {
+                  pname = "blinkstick";
+                  version = "2.0-dev";
 
-                src = pkgs.fetchFromGitHub {
-                  owner = "arvydas";
-                  repo = "blinkstick-python";
-                  rev = "8140b9fa18a9ff4f0e9df8e70c073f41cb8f1d35"; 
-                  sha256 = "02qfjvbinjid1hp5chi5ms3wpvfkbphnl2rcvdwwz5f87x63pdzm"; 
-                };
+                  src = pkgs.fetchFromGitHub {
+                    owner = "arvydas";
+                    repo = "blinkstick-python";
+                    rev = "95811252bc4129ee235dba3176be6987a0152c92";
+                    sha256 = "sha256-t+xn1zagpitxFxoybQK1uat2qFsggtye/OVrzZuWcX4=";
+                  };
 
-                propagatedBuildInputs = [ pkgs.python3Packages.pyusb ];
+                  propagatedBuildInputs = [pkgs.python3Packages.pyusb];
 
-                doCheck = false;
-                pythonImportsCheck = [ "blinkstick" ];
+                  pyproject = true;
+                  build-system = [pkgs.python3Packages.setuptools pkgs.python3Packages.setuptools-scm];
+                  doCheck = false;
+                  pythonImportsCheck = ["blinkstick"];
 
-                meta = with pkgs.lib; {
-                  description = "Python package to control BlinkStick USB devices";
-                  homepage = "https://github.com/arvydas/blinkstick-python";
-                  license = pkgs.lib.licenses.bsd3;
-                  maintainers = with pkgs.lib.maintainers; [ np ];
+                  meta = with pkgs.lib; {
+                    description = "Python package to control BlinkStick USB devices";
+                    homepage = "https://github.com/arvydas/blinkstick-python";
+                    license = licenses.bsd3;
+                    maintainers = with maintainers; [np];
+                  };
                 };
               };
-            };
           })
         ];
       };
 
-      blinkstick-python-env = pkgs.python3.withPackages (ps: [ pkgs.python3Packages.BlinkStick ]);
+      blinkstick-python-env = pkgs.python3.withPackages (ps: [pkgs.python3Packages.BlinkStick]);
 
       blinkstick-scripts = pkgs.writeScriptBin "blinkstick-scripts" ''
         #!${blinkstick-python-env.interpreter}
@@ -52,13 +60,13 @@
             num_leds = bstick.get_led_count()
 
             if sys.argv[1] == 'white':
-                led_data = [255, 255, 255] * num_leds  
+                led_data = [255, 255, 255] * num_leds
             elif sys.argv[1] == 'off':
-                led_data = [0, 0, 0] * num_leds  
+                led_data = [0, 0, 0] * num_leds
             elif sys.argv[1] == 'red':
-                led_data = [0, 255, 0] * num_leds  
+                led_data = [0, 255, 0] * num_leds
             elif sys.argv[1] == 'pink':
-                led_data = [105, 255, 180] * num_leds  
+                led_data = [105, 255, 180] * num_leds
             else:
                 print("Invalid argument")
                 sys.exit(1)
@@ -66,17 +74,16 @@
             bstick.set_led_data(0, led_data)
         else:
             print("No BlinkSticks found...")
-      ''; 
+      '';
 
-      scriptEnv = pkgs.python3.withPackages (ps: [ pkgs.python3Packages.BlinkStick ]);
-
+      scriptEnv = pkgs.python3.withPackages (ps: [pkgs.python3Packages.BlinkStick]);
     in {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.bashInteractive pkgs.python3 pkgs.python3Packages.BlinkStick ];
+        packages = [pkgs.bashInteractive pkgs.python3 pkgs.python3Packages.BlinkStick];
       };
 
-      packages = { 
-        blinkstick-scripts = blinkstick-scripts.overrideAttrs (old: { path = [ scriptEnv ]; });
+      packages = {
+        blinkstick-scripts = blinkstick-scripts.overrideAttrs (old: {path = [scriptEnv];});
       };
     });
 }
